0a26bf7705fb6aa4f17e180233490ad0eaa305a7
Tarbiee <dorcusstaby@gmail.com>
2024-01-15 13:29:45 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
@@ -16,8 +16,39 @@ db.init_app(app)
 
 api = Api(app)
 
-class Home(Resource):
-    pass
+class Newsletters(Resource):
+    def get(self):
+        newsletters=[]
+        for newsletter in Newsletter.query.all():
+            newsletter_dict = newsletter.to_dict()
+            newsletters.append(newsletter_dict)
+        response = make_response(
+            jsonify(newsletters),200,
+        )
+        return response
+    
+    def post(self):
+        data = request.get_json()
+        newsletter = Newsletter(
+            title = data['title'],
+            body=data['body']
+        )
+        db.session.add(newsletter)
+        db.session.commit()
+        return make_response(newsletter.to_dict(), 201,)
+    
+class NewslettersByID(Resource):
+    def get(self, id):
+        newsletter = Newsletter.query.filter_by(id=id).first()
+        newsletter_dict = newsletter.to_dict()
+        response = make_response(
+            jsonify(newsletter_dict),200,
+        )
+        return response
+
+
+api.add_resource(Newsletters, '/newsletters')
+api.add_resource(NewslettersByID, '/newsletters/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
